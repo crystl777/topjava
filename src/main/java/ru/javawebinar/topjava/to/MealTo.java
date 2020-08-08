7c5ed2320ca63193ff216dcddae419eb20ed96a4
Yakov Butenko <wx1324@yandex.ru>
2020-08-08 21:25:01 +0300

    9_07_update

--- src/main/java/ru/javawebinar/topjava/to/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/to/MealTo.java
@@ -14,6 +14,7 @@ public class MealTo {
 
     private final boolean excess;
 
+    @ConstructorProperties({"id", "dateTime", "description", "calories", "excess"})
     public MealTo(Integer id, LocalDateTime dateTime, String description, int calories, boolean excess) {
         this.id = id;
         this.dateTime = dateTime;
